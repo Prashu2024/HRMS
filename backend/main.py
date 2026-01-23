ab1c6e4c670b3cea6bcd256431b1e7cc8522d9ef
Prashant Gupta <159429540+Prashu2024@users.noreply.github.com>
2026-01-23 10:38:06 +0530

    Change allow_credentials to False in CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ print(allowed_origins)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
